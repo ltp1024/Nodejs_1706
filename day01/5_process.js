4386be020ca09b137e73609b9f53108063345389
thu <mingfei.net@gmail.com>
2017-11-10 11:56:20 +0800

    Update

---
+++ day01/5_process.js
@@ -0,0 +1,16 @@
+/**
+ * Created by web-01 on 2017/11/10.
+ */
+
+console.log(process.arch);
+process.kill(process.pid);
+console.log(process.platform);
+
+//console.log(process.env);
+//console.log(process.cwd()); // current working directory
+//console.log(process.execPath);
+//console.log(process.versions);
+//console.log(process.uptime());
+//console.log(process.memoryUsage());
+//console.log(process.pid);
+
