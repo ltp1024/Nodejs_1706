e2fe272f336cf5bc4f7578d3c05b23532fd88cbf
thu <mingfei.net@gmail.com>
2017-11-13 17:43:44 +0800

    Update

---
+++ day02/colors/color_test.js
@@ -0,0 +1,9 @@
+/**
+ * Created by web-01 on 2017/11/13.
+ */
+
+const colors = require('colors');
+
+console.log('red...'.red);
+console.log('green...'.green);
+console.log('blue...'.blue);
--- day02/mysql/mysql-test.js
+++ day02/mysql/mysql-test.js
@@ -3,3 +3,18 @@
  */
 
 const mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    user: 'root',
+    password: ''
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(connection.threadId);
+});
+
+connection.query('SELECT * FROM xuezi.user', (err, results, fields) => {
+    if(err) throw err;
+    console.log(results);
+});
