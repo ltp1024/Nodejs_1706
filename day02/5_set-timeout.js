c6537e2d2cd9bbe5d7c2b2082cb3463d6ff978c3
thu <mingfei.net@gmail.com>
2017-11-13 11:52:32 +0800

    Update

---
+++ day02/5_set-timeout.js
@@ -0,0 +1,11 @@
+/**
+ * Created by web-01 on 2017/11/13.
+ */
+
+console.log('start...'); // 1
+
+setTimeout(() => {
+    console.log('set timeout...'); // 2
+}, 1000);
+
+console.log('end...'); // 3
