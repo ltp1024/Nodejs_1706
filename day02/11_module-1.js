a4d68db11b2748d5c70adacf65966bad11ac4307
thu <mingfei.net@gmail.com>
2017-11-13 15:02:42 +0800

    Update

---
+++ day02/11_module-1.js
@@ -0,0 +1,6 @@
+/**
+ * Created by web-01 on 2017/11/13.
+ */
+
+let s1 = 's1...';
+exports.s1 = s1;
---
+++ day02/12_moudule-test.js
@@ -0,0 +1,5 @@
+/**
+ * Created by web-01 on 2017/11/13.
+ */
+let m1 = require('./11_module-1');
+console.log(m1.s1);
