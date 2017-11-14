c013bfb727e3651d48454cd9661ab38855280a62
thu <mingfei.net@gmail.com>
2017-11-14 10:19:55 +0800

    Update

---
+++ day03/path/test.js
@@ -0,0 +1,9 @@
+/**
+ * Created by web-01 on 2017/11/14.
+ */
+
+const path = require('path');
+
+console.log(path.normalize('/test1//test2'));
+console.log(path.join(__dirname, '../../test1/test2', 'test3/test4'));
+console.log(path.resolve('./test.js'));
