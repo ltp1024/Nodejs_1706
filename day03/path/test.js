3c8672c2ecdc2a4e736cd5ca8962bb359af0fea1
thu <mingfei.net@gmail.com>
2017-11-14 10:25:54 +0800

    Update

--- day03/path/test.js
+++ day03/path/test.js
@@ -7,3 +7,6 @@ const path = require('path');
 console.log(path.normalize('/test1//test2'));
 console.log(path.join(__dirname, '../../test1/test2', 'test3/test4'));
 console.log(path.resolve('./test.js'));
+
+console.log(path.isAbsolute(__filename));
+console.log(path.extname(__filename)); // extension name
