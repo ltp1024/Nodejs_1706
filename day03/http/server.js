1fc56b505ede88912ee8cb4d1a5327cd4b218e05
thu <mingfei.net@gmail.com>
2017-11-14 14:44:47 +0800

    Update

--- day03/http/server.js
+++ day03/http/server.js
@@ -4,8 +4,15 @@
 
 const http = require('http');
 
-let server = http.createServer((req, res) => {
-    res.end('server test...');
+let server = http.createServer();
+
+server.on('request', (req, res) => {
+    //console.log(req.url);
+    //console.log(req.httpVersion);
+    //console.log(req.headers);
+    //res.write('ok...')
+    //res.writeHeader(200, 'OK', {'content-type': 'txt/html; charset=utf-8'});
+    res.end('≤‚ ‘');
 });
 
 server.listen(80);
