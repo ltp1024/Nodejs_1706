34cc8132e41043677f77b91821f77ba15d5cb59c
thu <mingfei.net@gmail.com>
2017-11-14 09:27:50 +0800

    Update

--- day02/mysql/mysql-test.js
+++ day02/mysql/mysql-test.js
@@ -1,20 +1,37 @@
-/**
- * Created by web-01 on 2017/11/13.
- */
+///**
+// * Created by web-01 on 2017/11/13.
+// */
+//
+//const mysql = require('mysql');
+//
+//let connection = mysql.createConnection({
+//    user: 'root',
+//    password: ''
+//});
+//
+//connection.connect((err) => {
+//    if (err) throw err;
+//    console.log(connection.threadId);
+//});
+//
+//connection.query('SELECT * FROM xuezi.user', (err, results, fields) => {
+//    if(err) throw err;
+//    console.log(results);
+//});
+
 
 const mysql = require('mysql');
 
 let connection = mysql.createConnection({
-    user: 'root',
-    password: ''
+    user: 'root'
 });
 
 connection.connect((err) => {
-    if (err) throw err;
-    console.log(connection.threadId);
-});
-
-connection.query('SELECT * FROM xuezi.user', (err, results, fields) => {
     if(err) throw err;
-    console.log(results);
+    let sql = 'INSERT INTO xuezi.user VALUES(NULL, ?, ?, ?, ?, ?, ?, ?)'
+    connection.query(sql, ['a','b','c','d','e','f','g'], (err, results, fields) => {
+        if(err) throw err;
+        console.log(results.affectedRows);
+        console.log(results.insertId);
+    });
 });
