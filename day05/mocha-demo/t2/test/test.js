deee9a5ab86d768a4b332459778e82adef342cbc
thu <mingfei.net@gmail.com>
2017-11-16 16:01:42 +0800

    Update

--- day05/mocha-demo/t2/test/test.js
+++ day05/mocha-demo/t2/test/test.js
@@ -1,25 +1,43 @@
+const expect = require('chai').expect;
 
-
-
-var expect = require('chai').expect;
-
-describe('Class A', function () {
-    it('equal', function () {
+describe('Class A', () => {
+    it('equal', () => {
         expect(1 + 1).to.be.equal(2);
     });
-    it('no equal', function () {
+    it('no equal', () => {
         expect(1 - 1).to.be.not.equal(1);
     });
 });
 
-describe('Class B', function () {
-    it('true', function () {
+describe('Class B', () => {
+    it('true', () => {
         expect(1).to.be.ok;
     });
-    it('false', function () {
+    it('false', () => {
         expect(0).to.be.not.ok;
     });
-    it('false', function () {
+    it('false', () => {
         expect({}).to.be.ok;
     });
+});
+
+describe('Class C', () => {
+    it('type of string', () => {
+        expect('test...').to.be.a('string');
+    });
+    it('type of string', () => {
+        expect({}).to.be.an('object');
+    });
+});
+
+describe('Class D', () => {
+    it('include', () => {
+        expect([1, 2, 3]).to.include(1);
+    });
+    it('contain', () => {
+        expect('Hello, world!').to.contain('Hell');
+    });
+    it('include key', () => {
+        expect({k1: 'v1', k2: 'v2'}).to.include.keys('k1');
+    })
 });
