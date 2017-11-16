3c3333394de94af54237fbe5d17c93e3530f8d99
thu <mingfei.net@gmail.com>
2017-11-16 15:08:01 +0800

    Update

--- day05/mocha-demo/t1/add.js
+++ day05/mocha-demo/t1/add.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 function add(x, y) {
     return x + y;
 }
--- day05/mocha-demo/t1/add.test.js
+++ day05/mocha-demo/t1/add.test.js
@@ -1,10 +1,19 @@
+
+
+
+
+
 var add = require('./add');
 var expect = require('chai').expect;
 
-// console.log(add(1, 1));
-
-describe('A add() function Test', function () {
+describe('A add() Function Test', function () {
     it('1 + 1 = 2', function () {
         expect(add(1, 1)).to.be.equal(2);
-    })
+    });
+    it('1 + 0 = 1', function () {
+        expect(add(1, 0)).to.be.equal(1);
+    });
 });
+
+// decribe: test suite 测试套件
+// it: test case 测试用例
