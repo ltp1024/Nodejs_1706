b03ea6891206fd89f817f36ae0d1654809759b68
thu <mingfei.net@gmail.com>
2017-11-16 14:07:04 +0800

    Update

--- day04/webpack-demo/webpack.config.js
+++ day04/webpack-demo/webpack.config.js
@@ -3,5 +3,36 @@ module.exports = {
   output: {
     path: __dirname + "/public",
     filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: "./public",
+    historyApiFallback: true,
+    inline: true // 实时刷新
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "es2015",
+              "react"
+            ]
+          }
+        },
+        exclude: /node_modules/
+      }, {
+        test: /(\.css)$/,
+        use: [
+          {
+            loader: "style-loader"
+          }, {
+            loader: "css-loader"
+          }
+        ]
+      }
+    ]
   }
-}
+};
